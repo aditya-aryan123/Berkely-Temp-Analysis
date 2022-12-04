f0f7ede9639312251a905ecdf3f203961f8adad0
Aditya Aryan <38046685+aditya-aryan123@users.noreply.github.com>
2022-12-04 14:38:03 +0530

    Add files via upload

---
+++ cru.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from prophet import Prophet
+import seaborn as sns
+
+color_pal = sns.color_palette()
+plt.style.use('fivethirtyeight')
+
+df_berk = pd.read_csv('Avg_CRU.csv')
+df_berk['time'] = pd.to_datetime(df_berk['time'])
+
+df_copy = df_berk[['time', 'timeseries-tas-monthly-mean']]
+df_copy = df_copy.set_index('time')
+
+df_copy = df_copy.loc[df_copy.index > '1900-01-01', :]
+
+df_copy = df_copy.resample('M').mean()
+
+train_series = df_copy.loc[(df_copy.index >= '1900-01-01') & (df_copy.index < '2010-01-01'), 'timeseries-tas-monthly' \
+                                                                                             '-mean']
+test_series = df_copy.loc[(df_copy.index >= '2010-01-01'), 'timeseries-tas-monthly-mean']
+
+m = Prophet(seasonality_mode='additive', daily_seasonality=False, weekly_seasonality=False, yearly_seasonality=False)
+m.add_seasonality(name='monthly', period=365 * 12 * 12, fourier_order=5)
+train_series = train_series.reset_index().rename(columns={'time': 'ds'})
+train_series = train_series.rename(columns={'timeseries-tas-monthly-mean': 'y'})
+m.fit(train_series)
+future = m.make_future_dataframe(freq='M', periods=152)
+forecast = m.predict(future)
+pd.set_option('display.max_columns', 16)
+print(forecast.head())
+
+test_series = train_series.reset_index().rename(columns={'time': 'ds'})
+test_series = train_series.rename(columns={'timeseries-tas-monthly-mean': 'y'})
+df = test_series.merge(forecast[['yhat', 'yhat_lower', 'yhat_upper']], how='left', left_index=True,
+                       right_index=True)
+
+ax = df[['y']].plot(figsize=(15, 5))
+df['yhat'].plot(ax=ax)
+df['yhat_lower'].plot(ax=ax)
+df['yhat_upper'].plot(ax=ax)
+plt.legend(['Truth Data', 'Truth Data Upper', 'Truth Data Lower', 'Predictions'])
+ax.set_title('Raw Data and Prediction')
+plt.savefig("Prediction_yhat__CRU.png", dpi=300)
+
+fig1 = m.plot(forecast)
+fig1.savefig('Figure_1.png', dpi=300)
